818eaeecf057a01885935484fb0c7fa7f51dd943
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-05-04 19:01:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,12 +4,25 @@ const setBtnOpen = document.querySelector('.list__header__set');
 const setBtnClose = document.querySelector('.set__body__close');
 //change layout
 let root = document.documentElement;
+const setBody = document.querySelector('.set__body');
 const lightBtn = document.querySelector(
 	'.set__body__color__btn__opction--light'
 );
 const darkBtn = document.querySelector('.set__body__color__btn__opction--dark');
-const body = document.querySelector('body');
-//
+//chenge img main
+const pictureOne = document.querySelector(
+	'.set__body__img__opction__picture--one'
+);
+const pictureTwo = document.querySelector(
+	'.set__body__img__opction__picture--two'
+);
+const pictureThree = document.querySelector(
+	'.set__body__img__opction__picture--three'
+);
+const pictureFour = document.querySelector(
+	'.set__body__img__opction__picture--four'
+);
+const headerImg = document.querySelector('.list__header');
 
 const showSetPanel = () => {
 	setPanel.style.display = 'flex';
@@ -23,15 +36,30 @@ const checkLight = () => {
 	root.style.setProperty('--colorTextBlack', 'rgb(0, 0, 0)');
 	root.style.setProperty('--list-colorLight', 'rgb(255, 255, 255)');
 	root.style.setProperty('--toDo-light', 'rgb(235, 221, 221)');
+	root.style.setProperty('--set-color-light', 'rgb(199, 181, 181)');
+	lightBtn.classList.add('activ');
+	darkBtn.classList.remove('activ');
 };
 const checkDark = () => {
 	root.style.setProperty('--background-colorLight', 'rgb(70, 59, 59)');
 	root.style.setProperty('--colorTextBlack', 'rgb(255, 255, 255)');
 	root.style.setProperty('--list-colorLight', 'rgb(28, 25, 25)');
 	root.style.setProperty('--toDo-light', 'rgb(70, 59, 59)');
+	root.style.setProperty('--set-color-light', 'rgb(196, 156, 156)');
+
+	darkBtn.classList.add('activ');
+	lightBtn.classList.remove('activ');
+};
+
+const checkImg = (e) => {
+	if(pictureOne.e){console.log("dde");}
 };
 
 setBtnOpen.addEventListener('click', showSetPanel);
 setBtnClose.addEventListener('click', closeSetPanel);
 lightBtn.addEventListener('click', checkLight);
 darkBtn.addEventListener('click', checkDark);
+pictureOne.addEventListener('click',checkImg);
+pictureTwo.addEventListener('click',checkImg);
+pictureThree.addEventListener('click',checkImg);
+pictureFour.addEventListener('click',checkImg);
